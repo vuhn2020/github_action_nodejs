61d8628a35f31afdd5b624369c8b2e84587579bd
vuhn16@gmail.com <vuhn16@gmail.com>
2022-08-20 20:39:38 +0700

    add github action

--- app.js
+++ app.js
@@ -1,13 +1,74 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const prisma = new PrismaClient()
+const app = express()
+const port = 5000;
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/tutorial', async (req, res) => {
+  try {
+    const tutorials = await prisma.tutorial.findMany({
+      where: { published: true }
+    })
+    res.json(tutorials)
+  } catch {
+    res.status(404).send("Oh uh, something went wrong");
+  }
 })
-app.get('/names', (req, res) => {
-    res.send('names response')
-  })
+
+//routers and controllers
+app.post('/tutorial', async (req, res) => {
+  const { title, description } = req.body
+  try {
+    const tutorial = await prisma.tutorial.create({
+      data: {
+        title,
+        description,
+        published: true
+      },
+    })
+    res.json(tutorial)
+  } catch {
+    res.status(404).send("Oh uh, something went wrong");
+  }
+
+})
+
+app.put('/tutorial/:id', async (req, res) => {
+  const { title, description, published } = req.body
+  const { id } = req.params
+  try {
+    const tutorial = await prisma.tutorial.update({
+      where: { id: Number(id) },
+      data: {
+        title: title,
+        description: description,
+        published: published,
+      },
+    })
+    res.json(tutorial)
+  } catch {
+    res.status(404).send("Oh uh, something went wrong");
+  }
+})
+
+app.delete('/tutorial/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const tutorial = await prisma.tutorial.delete({
+      where: {
+        id: Number(id)
+      },
+    })
+    res.json(tutorial)
+  } catch {
+    res.status(404).send("Oh uh, something went wrong");
+  }
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ dockerfile
@@ -0,0 +1,22 @@
+#builder first
+FROM node:16-alpine
+# Create app directory
+WORKDIR /app
+COPY package.json package-lock.json ./
+COPY prisma ./prisma/
+
+# Install app dependencies
+RUN npm install
+
+# Generate prisma client
+RUN npx prisma generate
+
+# Migrate deploy
+RUN npx prisma migrate deploy
+
+# Bundle app source
+COPY . .
+EXPOSE 5000
+
+# Start server
+CMD ["startup.sh"]
