cf65b361e02924987fbeb45c7fc5c1e3b0345fe5
vuhn2020 <67276152+vuhn2020@users.noreply.github.com>
2022-08-21 17:39:55 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+echo "Shell bash is running!"
+#run prisma migrate deploy
+echo "prisma migrate is running!"
 npx prisma migrate deploy
+
+echo "npm start is running!"
 npm start
